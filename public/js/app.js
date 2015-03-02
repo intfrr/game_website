ce2315a32cce7d7140a833e42ef969dad2a429a3
Jordan Larson <jordan@jordonias.com>
2015-03-02 01:15:13 -0500

    add list view

--- public/js/app.js
+++ public/js/app.js
@@ -3,12 +3,47 @@
 angular.module('gameWebsiteApp', [
   'ui.router'
 ])
+
+/**
+ * Config
+ */
+
 .config(function($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise('/');
 
   $stateProvider
     .state('login', {
       url: '/',
-      templateUrl: 'login.html'
+      templateUrl: 'login.html',
+      controller: 'LoginController'
     })
-});
+    .state('list', {
+      url: '/list',
+      templateUrl: 'list.html',
+      controller: 'ListController'
+    });
+})
+
+/**
+ * Login Controller
+ */
+
+.controller('LoginController', [
+  '$scope',
+  '$state',
+function($scope, $state) {
+  $scope.login = function() {
+    $state.go('list');
+  }
+}])
+
+/**
+ * List Controller
+ */
+
+.controller('ListController', [
+  '$scope',
+  '$state',
+function($scope) {
+
+}]);
