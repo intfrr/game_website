ef2b3ce0c38fc2dfd00251d75109e87844865a14
Jordan Larson <jordan@jordonias.com>
2015-03-12 00:27:28 -0400

    add crude register, breaks login

--- api/user.js
+++ api/user.js
@@ -2,8 +2,11 @@
  * Module dependencies
  */
 
+var async = require('async');
+var crypto = require('crypto');
 var express = require('express');
 var passport = require('passport');
+var User = require('../lib/models').User;
 
 /**
  * Module variables
@@ -12,12 +15,91 @@ var passport = require('passport');
 var router = module.exports = express.Router();
 
 /**
- * /api/user/login
+ * POST /api/user/login
  */
 
 router.post('/login', passport.authenticate('local'), function(req, res) {
-  console.log(req.user);
   return res.json({
     status: 'OK',
+    result: req.user
+  });
+});
+
+/**
+ * POST /api/user
+ */
+
+router.post('/', function(req, res, next) {
+  res.status = 400;
+
+  if(typeof req.body.email === 'undefined') {
+    return next('Must specify an email.');
+  }
+
+  var email = req.body.email;
+
+  if(typeof req.body.password === 'undefined') {
+    return next('Must specify a password.');
+  }
+
+  if(typeof req.body.confirmPassword === 'undefined' ||
+      req.body.confirmPassword !== req.body.password) {
+    return next('Passwords must match.');
+  }
+
+  var password = req.body.password;
+  res.status = 200;
+
+  async.waterfall([
+    // Check if user already exists
+    function(cb) {
+      User.findOne({ email: req.body.email }, cb);
+    },
+    // Generate the salt
+    function(user, cb) {
+      if(user !== null) {
+        return cb('An account is already registered to that email.');
+      }
+
+      crypto.randomBytes(64, function(err, salt) {
+        if(err) {
+          return cb('Internal server error. Try again.');
+        }
+        cb(null, salt);
+      });
+    },
+    // Hash and salt the password
+    function(salt, cb) {
+      crypto.pbkdf2(password, salt, 8192, 512, function(err, key) {
+        if(err) {
+          return cb('Internal server error. Try again.');
+        }
+
+        var user = {
+          email: email,
+          password: key.toString('base64'),
+          salt: salt.toString('base64')
+        }
+
+        cb(null, user);
+      });
+    },
+    // Insert the user in the database
+    function(user, cb) {
+      var newUser = new User(user);
+
+      newUser.save(function(err, result) {
+        if(err) {
+          return cb('Internal server error. Try again.');
+        }
+
+        cb(null, user);
+      });
+    }
+  ], function(err, result) {
+    return res.json({
+      status: 'OK',
+      result: result
+    });
   });
 });
