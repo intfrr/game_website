716e0e183e857a6089d3622fcbc6e45948f40a16
Jordan Larson <jordan@jordonias.com>
2015-03-14 00:54:35 -0400

    client-side register validation

--- public/js/register.js
+++ public/js/register.js
@@ -15,6 +15,11 @@ angular.module('app.register', [
   '$state',
   '$resource',
 function($scope, $state, $resource) {
+  $scope.query = {
+    name: '',
+    password: ''
+  };
+
   // TODO: show/hide password to directive
   $scope.showPassword = function() {
     angular.element(document.querySelector('#password'))
@@ -34,12 +39,63 @@ function($scope, $state, $resource) {
       .css('color', '')
   }
 
-  $scope.register = function() {
-    $resource('/api/user').save($scope.query, function() {
-      $state.go('list');
+  $scope.emailConditions = [{
+    message: 'Valid email address',
+    test: function() {
+      // TODO: This isn't perfect
+      return /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
+        .test($scope.query.email);
+    }
+  }];
 
-    }, function(err) {
-      console.log(err);
-    });
+  $scope.passConditions = [{
+    message: 'Between 8 and 20 characters',
+    test: function() {
+      return $scope.query.password.length >= 8 &&
+        $scope.query.password.length <= 20
+    }
+  }, {
+    message: 'Contain only letters, digits, or any of !@#$%&*',
+    test: function() {
+      return /^[a-zA-Z0-9!@#$%&*]*$/
+        .test($scope.query.password);
+    }
+  }];
+
+  $scope.checkAllConditions = function() {
+    for(var i=0; i<$scope.emailConditions.length; i++) {
+      if(!$scope.emailConditions[i].test()) {
+        return false;
+      }
+    }
+    for(var i=0; i<$scope.passConditions.length; i++) {
+      if(!$scope.passConditions[i].test()) {
+        return false;
+      }
+    }
+    return true;
   }
+
+  $scope.register = function() {
+    if($scope.checkAllConditions()) {
+      $resource('/api/user').save($scope.query, function() {
+        $state.go('list');
+      }, function(err) {
+      });
+    } else {
+      angular.element(document.querySelector('#register-button'))
+        .addClass('btn-danger')
+        .removeClass('btn-default');
+
+      var unregister = $scope.$watch('checkAllConditions()', function(newValue, oldValue) {
+        if(newValue === true) {
+          angular.element(document.querySelector('#register-button'))
+            .addClass('btn-default')
+            .removeClass('btn-danger');
+          unregister();
+        }
+      });
+    }
+  }
+
 }])
